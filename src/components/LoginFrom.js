2b3862e203bbe48d1cbd2f7ba2eb3bb89f63280c
soskgo8830 <soskgo@gmail.com>
2024-02-01 17:45:18 +0900

    로그인 컴포넌트 추가 중

---
+++ src/components/LoginFrom.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+const LoginFrom = () => {
+  return (
+    <div>
+      <h2>LoginFrom</h2>
+      <Form
+        name='basic'
+        layout='vertical'
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
+      >
+        <Form.Item
+          label='Email'
+          name='email'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Password'
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name='remember' valuePropName='checked'>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginFrom;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import LoginFrom from '../components/LoginFrom';
 
-const StyledLoginLayout = styled.div``;
+const StyledLoginLayout = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  height: 100vh;
+  background-color: #2f3249;
+`;
+
+const StyledLeft = styled.div`
+  background-color: #25293d;
+  margin: 2rem;
+  border-radius: 1rem;
+`;
+
+const StyledRight = styled.div`
+  margin: 3rem;
+`;
 
 const LoginPage = () => {
-  return <StyledLoginLayout></StyledLoginLayout>;
+  return (
+    <StyledLoginLayout>
+      <StyledLeft></StyledLeft>
+      <StyledRight>
+        <LoginFrom></LoginFrom>
+      </StyledRight>
+    </StyledLoginLayout>
+  );
 };
 
 export default LoginPage;
