00fc39f0bd9468db20545453c7abe0fe6552442f
soskgo8830 <soskgo@gmail.com>
2024-02-23 01:42:25 +0900

    파이어베이스 의존성 제거

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import LoginPage from './pages/LoginPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import SignUpPage from './pages/SignUpPage';
 import MainPage from './pages/MainPage';
-
-import { firestore } from './firebase.js';
-import { useEffect } from 'react';
-
 function App() {
-  useEffect(() => {
-    console.log(firestore);
-  });
   return (
     <BrowserRouter>
       <Routes>
