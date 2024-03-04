b290c620b3087243be9f2bc0aec30b1128beec68
soskgo8830 <soskgo@gmail.com>
2024-03-04 15:51:54 +0900

    풀캘린더 추가 및 기본 설정

--- src/components/calender/CalendarMain.js
+++ src/components/calender/CalendarMain.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import { Calendar } from 'antd';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 const CalendarMain = () => {
-  return <Calendar className='padding-sm'></Calendar>;
+  const headerToolbar = {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+  };
+
+  return (
+    <div style={{ width: '100%' }} className='padding-lg'>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+        initialView={'dayGridMonth'}
+        weekends={true}
+        eventContent={renderEventContent}
+        headerToolbar={headerToolbar} // 커스텀 헤더 설정
+        events={[
+          {
+            title: '이벤트1',
+            date: '2024-03-01',
+            start: '2024-03-01 12:11',
+            end: '2024-03-04 14:24',
+            backgroundColor: '#2f3249',
+            borderColor: '#2f3249',
+          },
+          { title: '이벤트2', date: '2024-03-02' },
+        ]}
+      />
+    </div>
+  );
 };
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 
 export default CalendarMain;
