5e446642f67b9569c005ca6242283199fa5cb600
soskgo8830 <soskgo@gmail.com>
2024-03-14 23:57:32 +0900

    캘린더 추가 모달 input 추가

--- src/components/calendar/AddCalendarModal.js
+++ src/components/calendar/AddCalendarModal.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Form, Input, Modal, Button } from 'antd';
+import { Form, Input, Modal, Button, DatePicker, Select } from 'antd';
+const { TextArea } = Input;
+const { RangePicker } = DatePicker;
+const { Option } = Select;
 
 const AddCalendarModal = ({
   isModalOpen,
@@ -10,7 +13,36 @@ const AddCalendarModal = ({
     setIsModalOpen(false);
   };
 
-  const onFinish = async (values) => {};
+  const onFinish = async (values) => {
+    console.log(values);
+  };
+
+  function handleChange(value) {
+    console.log(`Selected: ${value}`);
+  }
+
+  const categorys = [
+    {
+      id: 'ca1',
+      name: 'Personal',
+      color: '#ea5456',
+    },
+    {
+      id: 'ca2',
+      name: 'Business',
+      color: '#fe9e41',
+    },
+    {
+      id: 'ca3',
+      name: 'Holiday',
+      color: '#28c76f',
+    },
+    {
+      id: 'ca4',
+      name: 'ETC',
+      color: '#11cee5',
+    },
+  ];
 
   return (
     <Modal
@@ -36,6 +68,68 @@ const AddCalendarModal = ({
           ]}
         >
           <Input />
+        </Form.Item>
+
+        <div className='form-input-header'>Date</div>
+        <Form.Item
+          name='date'
+          rules={[
+            {
+              required: true,
+              message: 'Please input Date.',
+            },
+          ]}
+        >
+          <DatePicker style={{ width: '100%' }} />
+        </Form.Item>
+
+        <div className='form-input-header'>Range Date</div>
+        <Form.Item
+          name='rangeDate'
+          rules={[
+            {
+              required: true,
+              message: 'Please input Range Date.',
+            },
+          ]}
+        >
+          <RangePicker showTime style={{ width: '100%' }} />
+        </Form.Item>
+
+        <div className='form-input-header'>Contents</div>
+        <Form.Item
+          name='contents'
+          rules={[
+            {
+              required: true,
+              message: 'Please enter your contents.',
+            },
+          ]}
+        >
+          <TextArea rows={6} />
+        </Form.Item>
+
+        <div className='form-input-header'>categorys</div>
+        <Form.Item
+          name='categorys'
+          rules={[
+            {
+              required: true,
+              message: 'Please enter your categorys.',
+            },
+          ]}
+        >
+          <Select style={{ width: '100%' }} onChange={handleChange}>
+            {categorys.map((category) => (
+              <Option
+                key={category.id}
+                value={category.id}
+                style={{ color: category.color }}
+              >
+                {category.name}
+              </Option>
+            ))}
+          </Select>
         </Form.Item>
 
         <Form.Item>
